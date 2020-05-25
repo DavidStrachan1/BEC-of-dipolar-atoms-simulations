4cc094bed234e3a7d589492678983cf9dc0f6232
EdwardG13 <65567827+EdwardG13@users.noreply.github.com>
2020-05-25 23:35:34 +0100

    Update gpe1d.py

--- gpe1d.py
+++ gpe1d.py
@@ -4,7 +4,8 @@ Created on Fri May 15 20:50:35 2020
 
 @author: ganda
 
-Solution of time independant Schrödinger equation using imaginary time propagation
+Modelling a dipolar Bose-Einstein condensate by finding the solution of the 
+time dependant Schrödinger equation using imaginary time propagation
 and the split step Fourier method in 1D
 """
 import numpy as np
@@ -16,43 +17,49 @@ import warnings
 warnings.simplefilter('ignore')
 
 # Defining size of x space
-x=np.linspace(-5,5,101)
+x=np.linspace(-10,10,1000)
 N=len(x)
-dx=(x[N-1]-x[0])/(N-1)
-dt=-0.1j # Imaginary time propagation
+dx=(x[N-1]-x[0])/N
+# Imaginary time propagation (dt<<ts for convergence)
+# ts=1/w for harmonic potential (for w= 20π, ts=0.016) 
+#and ts=mL^2/hbar for a box (For L=20, ts=0.8)
+dt=-0.005j
 
 # Setting up k space
 k=fftfreq(N,dx/(2*cn.pi)) 
 
 # Guess initial psi as gaussian
-#psi=0.5*np.exp(-x**2)
-psi=np.ones(N)
+psi=0.5*np.exp(-x**2)
+#psi=np.ones(N)
 #psi=x**2
 #psi=np.cos(cn.pi*x/20)
 
-
-# Defining potential operator V
-w=1 # Angular velocity
-g=0 # Contact coefficient
-Cdd=0 # Dipole-dipole interaction coefficient
+# Units and dimensional stuff
+m=2.7774e-25 # Typical mass of erbium
+a=5.1e-9 # Typical s-wave scattering length
+w=20*cn.pi # Typical angular velocity
+xs=np.sqrt(cn.hbar/(m*w)) # Length scaling parameter
+n=1e5 # Typical number of particles
+gs=4*cn.pi*a*n/xs # Dimensionless contact coefficient
+bohr_mag=cn.e*cn.hbar/(2*cn.m_e) # Bohr magneton
+#Cdd=cn.mu_0*(6.98*bohr_mag)**2/(4*cn.pi) # Dipole-dipole interaction coefficient
+Cdd=0
 
 def Vdd(psi): # Dipole interaction energy
     Rc=10 # Circular cut off should be greater than system size
     Uddf=1/3*Cdd*np.nan_to_num((1+3*np.cos(Rc*k)/(Rc*k)**2-3*np.sin(Rc*k)/(Rc*k)**3)\
         *(3*(0)**2 -1)) # FT of the dipole energy. Assumes polarsation along x
+    return ifft(Uddf*fft(np.abs(psi)**2))     
     
-    return ifft(Uddf*fft(np.abs(psi)**2))
 
-#def expVh(psi): # Harmonic potential
- #   V=0.5*w**2*x**2 + g*np.abs(psi)**2 + Vdd(psi)
-  #  return np.exp(-0.5j*V*dt)
+def expVh(psi): # Harmonic potential
+    V=0.5*x**2 + gs*np.abs(psi)**2 + Vdd(psi)
+    return np.exp(-0.5j*V*dt)
   
 
-def expVh(psi): # Box potential
-
-    V=((1/4.75)*x)**1000 + g*np.abs(psi)**2 + Vdd(psi)
-    return np.exp(-0.5j*V*dt)
-
+#def expVh(psi): # Box potential
+ #   V=(x/9.9)**1000 + gs*np.abs(psi)**2 + Vdd(psi)
+  #  return np.exp(-0.5j*V*dt)
 
 # Defining kinetic energy operator T
 T=0.5*k**2
@@ -66,7 +73,7 @@ histpsi=[[0,psi[0],psi[int(round(N/2))],psi[N-1]]]
 isConv=0;
 i=1;
 
-while isConv==0 and i<100: # Loop until convergence or limit reached
+while isConv==0 and i<1000: # Loop until convergence or limit reached
     
     psi=expVh(psi)*ifft(expT*fft(expVh(psi)*psi)) # Split step Fourier method
     psi/=la.norm(psi)
@@ -87,13 +94,13 @@ yhist=[i[2] for i in histpsi]
 
 # Plotting results
     
-#actual=np.exp(-0.5*x**2)
-actual=np.cos(cn.pi*x/10)
+actual=np.exp(-0.5*x**2)
+#actual=np.cos(cn.pi*x/20)
 actual/=la.norm(actual)
 diff=psi-actual
 plt.plot(x,psi,color="k",label="Numerical result")
 plt.plot(x,actual,color="g",label="Actual result") 
 #plt.plot(x,diff,color="orange",label="Difference between numerical and actual results")  
-plt.xlabel("Distance")
+plt.xlabel("Distance (μm)")
 plt.ylabel("Wavefunction")
 plt.legend(loc="upper right")
